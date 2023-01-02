438adf6ed69a20df40abfd896ae3fdd07fb7952d
Conor Mancone <cmancone@gmail.com>
2023-01-02 15:26:43 -0500

    Cleanup

--- main.tf
+++ main.tf
@@ -11,10 +11,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "test_instance" {
-  source = "./ec2_instance"
-  ami = data.aws_ami.ubuntu.id
-  subnet_id = module.vpc.public_subnets[0]
-  vpc_id = module.vpc.vpc_id
-  name = "test-instance"
-}
+#module "test_instance" {
+#  source = "./ec2_instance"
+#  ami = data.aws_ami.ubuntu.id
+#  subnet_id = module.vpc.public_subnets[0]
+#  vpc_id = module.vpc.vpc_id
+#  name = "test-instance"
+#}
